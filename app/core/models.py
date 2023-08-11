4563b7f57e45b9c7127bcbc12332dbf67271b400
Justice Arthur <justicea83@gmail.com>
2023-08-11 11:10:32 +0000

    add tag model

--- app/core/models.py
+++ app/core/models.py
@@ -56,3 +56,15 @@ class Recipe(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    """Tag for filtering recipes."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
